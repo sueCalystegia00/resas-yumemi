85a339cb2c3bd9464b61cf5e7594df5dc3c57893
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-04 14:20:05 +0900

    update: 都道府県一覧の取得処理と動作確認クリア

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,32 @@
+import axios from "axios";
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    error: false,
+    prefectures: [],
+  },
+  mutations: {
+    setError: (state, payload) => (state.error = payload),
+    setPrefectures: (state, payload) => (state.prefectures = payload),
+  },
+  actions: {
+    getPrefectures: async (context) => {
+      // 都道府県一覧の取得
+      const response = await axios.get(
+        "https://opendata.resas-portal.go.jp/api/v1/prefectures",
+        {
+          headers: { "X-API-KEY": process.env.VUE_APP_RESAS_API_KEY },
+        }
+      );
+      if (response.data.message === null) {
+        // 正常
+        context.commit("setPrefectures", response.data.result);
+      } else {
+        // API通信失敗
+        context.commit("setError", true);
+      }
+    },
+  },
   modules: {},
 });
