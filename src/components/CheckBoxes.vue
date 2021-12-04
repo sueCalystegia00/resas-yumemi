3f57395d9b10ff853d39fa17533d09dec4dfe8a4
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-04 14:37:58 +0900

    add: チェックボックスのコンポーネントを作成

---
+++ src/components/CheckBoxes.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="boxes">{{ prefectures }}</div>
+</template>
+
+<script>
+export default {
+  name: "CheckBoxes",
+  props: {
+    prefectures: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="home">{{ prefectures }}</div>
+  <div class="home">
+    <CheckBoxes :prefectures="prefectures" />
+  </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import CheckBoxes from "/src/components/CheckBoxes";
 export default {
   name: "Home",
+  components: {
+    CheckBoxes,
+  },
   computed: {
     ...mapState(["prefectures"]),
   },
