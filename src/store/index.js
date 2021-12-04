b1e77884de3ef3f52f4c4cd9e6b0b0fbc5e4d8a9
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-04 17:20:34 +0900

    update: APIから総人口を取得，格納する処理を追加

--- src/store/index.js
+++ src/store/index.js
@@ -21,11 +21,12 @@ export default createStore({
   mutations: {
     setError: (state, payload) => (state.error = payload),
     setPrefectures: (state, payload) => (state.prefectures = payload),
+    setPopulation: (state, payload) => state.population.push(payload),
   },
   actions: {
     /**
      * RESAS API から都道府県一覧を取得
-     * @param {*} context
+     * @param {any(VuexContext)} context
      */
     getPrefectures: async (context) => {
       // 都道府県一覧の取得
@@ -38,6 +39,37 @@ export default createStore({
       if (response.data.message === null) {
         // 正常
         context.commit("setPrefectures", response.data.result);
+      } else {
+        // API通信失敗
+        context.commit("setError", true);
+      }
+    },
+    /**
+     * RESAS API から指定の都道府県の総人口推移データを取得
+     * @param {any(VuexContext)} context
+     * @param {object} payload // 都道府県名，都道府県コードを持つオブジェクト
+     */
+    getPopulation: async (context, payload) => {
+      // 人口構成データの取得
+      const response = await axios.get(
+        `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${payload.prefCode}`,
+        {
+          headers: { "X-API-KEY": process.env.VUE_APP_RESAS_API_KEY },
+        }
+      );
+      if (response.data.message === null) {
+        // 正常
+        // 総人口の値のみを配列として取り出す
+        const populationData = response.data.result.data[0].data.map(
+          (x) => x.value
+        );
+        const dataset = {
+          [payload.prefCode]: {
+            prefName: payload.prefName,
+            data: populationData,
+          },
+        };
+        context.commit("setPopulation", dataset);
       } else {
         // API通信失敗
         context.commit("setError", true);
