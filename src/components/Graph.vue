6129ce67135eb78be51dc5bede777b1c55eb9c89
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-05 21:20:57 +0900

    update: グラフデザイン

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="graph">
+    <transition name="fade">
+      <div v-show="!seriesData.length" class="dialog">
+        <h3>👇 都道府県を下から選んでね</h3>
+        <p>人口グラフが表示されるよ</p>
+      </div>
+    </transition>
     <vue-highcharts
       type="chart"
       :options="chartOptions"
@@ -33,28 +39,44 @@ export default {
     chartOptions() {
       return {
         title: {
-          text: "総人口推移",
+          text: "都道府県別 総人口の推移",
           style: {
             "font-family": "Avenir, Helvetica, Arial, sans-serif",
             "-webkit-font-smoothing": "antialiased",
             "-moz-osx-font-smoothing": "grayscale",
             color: "#2c3e50",
             "font-weight": "bold",
-            "font-size": "200%",
+            "font-size": "1.5rem",
           },
         },
         yAxis: {
           title: {
-            text: "人口数",
+            text: "人口(百万人)",
+            align: "high",
+            rotation: 0,
+            offset: -40,
+            x: 0,
+            y: -10,
           },
-          tickInterval: 500000,
+          labels: {
+            formatter: function () {
+              return this.value / 1000000;
+            },
+          },
         },
         xAxis: {
-          type: "datetime",
-          tickInterval: 5,
-          labels: {
-            rotation: 60,
+          title: {
+            text: "(年)",
+            align: "high",
+            opposite: false,
           },
+          tickInterval: 1,
+          categories: [...Array(18)].map((_, index) => 1980 + index * 5),
+        },
+        tooltip: {
+          headerFormat: "{point.x}年<br>",
+          pointFormat: "{series.name}: {point.y}人",
+          crosshairs: true,
         },
         series: this.seriesData,
       };
@@ -62,10 +84,10 @@ export default {
   },
   methods: {
     onRender() {
-      console.log("Chart rendered");
+      //console.log("Chart rendered");
     },
     onUpdated() {
-      console.log("Chart updated");
+      //console.log("Chart updated");
       this.reloadKey = !this.reloadKey;
     },
   },
@@ -78,5 +100,43 @@ export default {
   width: 100%;
   padding: 10px;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.vue-highcharts {
+  width: 100%;
+  height: 400px;
+  box-sizing: border-box;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 500ms 0s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.dialog {
+  position: fixed;
+  top: 55px;
+  width: 100%;
+  height: 340px;
+  box-sizing: border-box;
+  z-index: 9999;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #d7d7d784;
+  border-radius: 10px;
+}
+h3,
+p {
+  margin: 0;
 }
 </style>
